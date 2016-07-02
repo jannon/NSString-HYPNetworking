6c1a18f2bf8559882c9e5ff00d48749ae23237d1
Jannon Frank <jannon@cs.stanford.edu>
2016-07-02 02:43:05 -0700

    change import

--- NSString_HYPNetworking/NSString_HYPNetworking.h
+++ NSString_HYPNetworking/NSString_HYPNetworking.h
@@ -4,4 +4,4 @@ FOUNDATION_EXPORT double NSString_HYPNetworkingVersionNumber;
 FOUNDATION_EXPORT const unsigned char NSString_HYPNetworkingVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <NSString_HYPNetworking/PublicHeader.h>
-#import <NSString_HYPNetworking/NSString+HYPNetworking.h>
+#import "NSString+HYPNetworking.h"
